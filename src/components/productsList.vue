f32ab949e9c69227a16193f2f4b01eef9df00121
alex <alex.sanders@ukr.net>
2025-02-27 17:55:28 +0200

    Update productsList.vue

--- src/components/productsList.vue
+++ src/components/productsList.vue
@@ -38,7 +38,7 @@ watchEffect(fetchProducts);
     class="w-full max-w-3xl m-4 rounded-md bg-white p-2 mb-4 outline-none"
   />
 
-  <div class="w-full max-w-3xl h-[400px] overflow-auto m-4 space-y-4 p-4 bg-white rounded-md">
+  <div class="w-full max-w-3xl h-[600px] overflow-auto m-4 space-y-4 p-4 bg-white rounded-md">
     <ProductItem v-for="product in products" :key="product.id" :product="product" />
   </div>
 </template>
